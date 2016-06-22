19e28e5498a8f979ac190fcf007da5d8f61da7aa
Rustam Makulov <rmakulov@users.noreply.github.com>
2016-06-22 18:19:32 +0300

    register to avoid collisions (#10)

--- src/test/java/com/axibase/tsd/api/method/series/CSVInsertTest.java
+++ src/test/java/com/axibase/tsd/api/method/series/CSVInsertTest.java
@@ -1,5 +1,7 @@
 package com.axibase.tsd.api.method.series;
 
+import com.axibase.tsd.api.model.entity.Entity;
+import com.axibase.tsd.api.model.metric.Metric;
 import com.axibase.tsd.api.model.series.SeriesQuery;
 import junit.framework.Assert;
 import org.junit.Test;
@@ -11,23 +13,24 @@ public class CSVInsertTest extends CSVInsertMethod {
     /* #2009 */
     @Test
     public void testISOFormatZNoMS() throws Exception {
-        String entity = "e-iso-5";
-        String metric = "m-iso-5";
+        Entity entity = new Entity("e-iso-5");
+        Metric metric = new Metric("m-iso-5");
         Map tags = new HashMap<String, String>();
         tags.put("tag-1", "value-1");
         tags.put("tag-2", "value-2");
 
-        String csvPayload = "date,m-iso-5\n" +
+        String csvPayload = "date," + metric.getName() + "\n" +
                 "2016-05-21T00:00:00Z, 12.45\n" +
                 "2016-05-21T00:00:15Z, 10.8";
-        csvInsert(entity, csvPayload, tags, 1000);
+        csvInsert(entity.getName(), csvPayload, tags, 1000);
 
-        SeriesQuery seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:00Z", "2016-05-21T00:00:01Z", tags);
+        SeriesQuery seriesQuery = new SeriesQuery(entity.getName(), metric.getName(),
+                "2016-05-21T00:00:00Z", "2016-05-21T00:00:01Z", tags);
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:00.000Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "12.45", getDataField(0, "v"));
 
-        seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:15Z", "2016-05-21T00:00:16Z");
+        seriesQuery = new SeriesQuery(entity.getName(), metric.getName(), "2016-05-21T00:00:15Z", "2016-05-21T00:00:16Z");
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:15.000Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "10.8", getDataField(0, "v"));
@@ -36,23 +39,25 @@ public class CSVInsertTest extends CSVInsertMethod {
     /* #2009 */
     @Test
     public void testISOFormatZMS() throws Exception {
-        String entity = "e-iso-6";
-        String metric = "m-iso-6";
+        Entity entity = new Entity("e-iso-6");
+        Metric metric = new Metric("m-iso-6");
         Map tags = new HashMap<String, String>();
         tags.put("tag-1", "value-1");
         tags.put("tag-2", "value-2");
 
-        String csvPayload = "date,m-iso-6\n" +
+        String csvPayload = "date," + metric.getName() + "\n" +
                 "2016-05-21T00:00:00.001Z, 12.45\n" +
                 "2016-05-21T00:00:15.001Z, 10.8";
-        csvInsert(entity, csvPayload, tags, 1000);
+        csvInsert(entity.getName(), csvPayload, tags, 1000);
 
-        SeriesQuery seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:00.001Z", "2016-05-21T00:00:00.002Z", tags);
+        SeriesQuery seriesQuery = new SeriesQuery(entity.getName(), metric.getName(),
+                "2016-05-21T00:00:00.001Z", "2016-05-21T00:00:00.002Z", tags);
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:00.001Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "12.45", getDataField(0, "v"));
 
-        seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:15.001Z", "2016-05-21T00:00:15.002Z");
+        seriesQuery = new SeriesQuery(entity.getName(), metric.getName(),
+                "2016-05-21T00:00:15.001Z", "2016-05-21T00:00:15.002Z");
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:15.001Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "10.8", getDataField(0, "v"));
@@ -61,23 +66,23 @@ public class CSVInsertTest extends CSVInsertMethod {
     /* #2009 */
     @Test
     public void testISOFormatPlusHourNoMS() throws Exception {
-        String entity = "e-iso-7";
-        String metric = "m-iso-7";
+        Entity entity = new Entity("e-iso-7");
+        Metric metric = new Metric("m-iso-7");
         Map tags = new HashMap<String, String>();
         tags.put("tag-1", "value-1");
         tags.put("tag-2", "value-2");
 
-        String csvPayload = "date,m-iso-7\n" +
+        String csvPayload = "date," + metric.getName() + "\n" +
                 "2016-05-21T00:00:00+00:00, 12.45\n" +
                 "2016-05-21T00:00:15+00:00, 10.8";
-        csvInsert(entity, csvPayload, tags, 1000);
+        csvInsert(entity.getName(), csvPayload, tags, 1000);
 
-        SeriesQuery seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:00Z", "2016-05-21T00:00:10Z", tags);
+        SeriesQuery seriesQuery = new SeriesQuery(entity.getName(), metric.getName(), "2016-05-21T00:00:00Z", "2016-05-21T00:00:10Z", tags);
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:00.000Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "12.45", getDataField(0, "v"));
 
-        seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:15Z", "2016-05-21T00:00:20Z");
+        seriesQuery = new SeriesQuery(entity.getName(), metric.getName(), "2016-05-21T00:00:15Z", "2016-05-21T00:00:20Z");
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:15.000Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "10.8", getDataField(0, "v"));
@@ -86,23 +91,24 @@ public class CSVInsertTest extends CSVInsertMethod {
     /* #2009 */
     @Test
     public void testISOFormatPlusHourMS() throws Exception {
-        String entity = "e-iso-8";
-        String metric = "m-iso-8";
+        Entity entity = new Entity("e-iso-8");
+        Metric metric = new Metric("m-iso-8");
         Map tags = new HashMap<String, String>();
         tags.put("tag-1", "value-1");
         tags.put("tag-2", "value-2");
 
-        String csvPayload = "date,m-iso-8\n" +
+        String csvPayload = "date," + metric.getName() + "\n" +
                 "2016-05-21T00:00:00.001+00:00, 12.45\n" +
                 "2016-05-21T00:00:15.001+00:00, 10.8";
-        csvInsert(entity, csvPayload, tags, 1000);
+        csvInsert(entity.getName(), csvPayload, tags, 1000);
 
-        SeriesQuery seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:00.001Z", "2016-05-21T00:00:00.002Z", tags);
+        SeriesQuery seriesQuery = new SeriesQuery(entity.getName(), metric.getName(), "2016-05-21T00:00:00.001Z", "2016-05-21T00:00:00.002Z", tags);
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:00.001Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "12.45", getDataField(0, "v"));
 
-        seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:15.001Z", "2016-05-21T00:00:15.002Z");
+        seriesQuery = new SeriesQuery(entity.getName(), metric.getName(),
+                "2016-05-21T00:00:15.001Z", "2016-05-21T00:00:15.002Z");
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:15.001Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "10.8", getDataField(0, "v"));
@@ -111,36 +117,36 @@ public class CSVInsertTest extends CSVInsertMethod {
     /* #2009 */
     @Test
     public void testMultipleISOFormat() throws Exception {
-        String entity = "e-iso-9";
-        String metric = "m-iso-9";
+        Entity entity = new Entity("e-iso-9");
+        Metric metric = new Metric("m-iso-9");
         Map tags = new HashMap<String, String>();
         tags.put("tag-1", "value-1");
         tags.put("tag-2", "value-2");
 
-        String csvPayload = "date,m-iso-9\n" +
+        String csvPayload = "date," + metric.getName() + "\n" +
                 "2016-05-21T00:00:00Z,      12.45\n" +
                 "2016-05-21T00:00:00.001Z,      12\n" +
                 "2016-05-21T00:00:15+00:00, 10.8\n" +
                 "2016-05-21T00:00:15.001+00:00, 10";
-        csvInsert(entity, csvPayload, tags, 1000);
+        csvInsert(entity.getName(), csvPayload, tags, 1000);
 
-        SeriesQuery seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:00Z", "2016-05-21T00:00:10Z", tags);
+        SeriesQuery seriesQuery = new SeriesQuery(entity.getName(), metric.getName(), "2016-05-21T00:00:00Z", "2016-05-21T00:00:10Z", tags);
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:00.000Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "12.45", getDataField(0, "v"));
 
-        seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:00.001Z", "2016-05-21T00:00:00.002Z", tags);
+        seriesQuery = new SeriesQuery(entity.getName(), metric.getName(), "2016-05-21T00:00:00.001Z", "2016-05-21T00:00:00.002Z", tags);
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:00.001Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "12", getDataField(0, "v"));
 
 
-        seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:15Z", "2016-05-21T00:00:20Z");
+        seriesQuery = new SeriesQuery(entity.getName(), metric.getName(), "2016-05-21T00:00:15Z", "2016-05-21T00:00:20Z");
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:15.000Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "10.8", getDataField(0, "v"));
 
-        seriesQuery = new SeriesQuery(entity, metric, "2016-05-21T00:00:15.001Z", "2016-05-21T00:00:15.002Z");
+        seriesQuery = new SeriesQuery(entity.getName(), metric.getName(), "2016-05-21T00:00:15.001Z", "2016-05-21T00:00:15.002Z");
         executeQuery(seriesQuery);
         Assert.assertEquals("Stored date incorrect", "2016-05-21T00:00:15.001Z", getDataField(0, "d"));
         Assert.assertEquals("Stored value incorrect", "10", getDataField(0, "v"));
