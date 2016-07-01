b6279df5974904aeffa02ed4fb670009cdc42f8c
Dmitry <failsild@gmail.com>
2016-07-01 11:17:24 +0300

    change http_client&logger filter (#26)

--- src/test/java/com/axibase/tsd/api/method/BaseMethod.java
+++ src/test/java/com/axibase/tsd/api/method/BaseMethod.java
@@ -3,8 +3,10 @@ package com.axibase.tsd.api.method;
 import com.axibase.tsd.api.Config;
 import com.axibase.tsd.api.transport.tcp.TCPSender;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.json.JSONArray;
@@ -25,7 +27,6 @@ import java.lang.invoke.MethodHandles;
 import java.text.SimpleDateFormat;
 import java.util.logging.Filter;
 import java.util.logging.Level;
-import java.util.logging.LogRecord;
 
 
 public abstract class BaseMethod {
@@ -55,6 +56,7 @@ public abstract class BaseMethod {
             clientConfig.register(httpAuthenticationFeature);
             clientConfig.register(MultiPartFeature.class);
             clientConfig.register(new LoggingFeature());
+            clientConfig.connectorProvider(new ApacheConnectorProvider());
             httpRootResource = ClientBuilder.newClient(clientConfig).target(UriBuilder.fromPath("")
                     .scheme(config.getProtocol())
                     .host(config.getServerName())
