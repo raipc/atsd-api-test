1b212aa5df990f897e950525a0b8183fcf97d699
Igor Shmagrinskiy <unrealwork@gmail.com>
2016-07-13 14:49:43 +0300

    squashed (#51)

--- src/test/java/com/axibase/tsd/api/method/sql/SqlTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/SqlTest.java
@@ -1,8 +1,11 @@
 package com.axibase.tsd.api.method.sql;
 
+import com.axibase.tsd.api.model.sql.ColumnMetaData;
 import com.axibase.tsd.api.model.sql.StringTable;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,5 +23,24 @@ public class SqlTest extends SqlMethod {
 
     public void assertTableContainsColumnValues(List<String> values, StringTable table, String columnName) {
         assertEquals(String.format("Values of column with name: %s are not equal to expected", columnName), table.columnValues(columnName), values);
+    }
+
+    public void assertTableColumnsNames(List<String> expectedColumnsNames, StringTable table) {
+        Set<String> resultColumnNames = extractColumnNames(table.getColumnsMetaData());
+        assertEquals("Table columns names are not equal to expected", new HashSet<>(expectedColumnsNames), resultColumnNames);
+    }
+
+    /**
+     * Retrieve column names form table column metadata set
+     *
+     * @param columnMetaData set of column metadata values
+     * @return column names set
+     */
+    private Set<String> extractColumnNames(Set<ColumnMetaData> columnMetaData) {
+        Set<String> columnNames = new HashSet<>();
+        for (ColumnMetaData data : columnMetaData) {
+            columnNames.add(data.getName());
+        }
+        return columnNames;
     }
 }
---
+++ src/test/java/com/axibase/tsd/api/method/sql/select/SqlSelectAllWithJoinTests.java
@@ -0,0 +1,125 @@
+package com.axibase.tsd.api.method.sql.select;
+
+import com.axibase.tsd.api.Util;
+import com.axibase.tsd.api.method.sql.SqlTest;
+import com.axibase.tsd.api.model.series.Sample;
+import com.axibase.tsd.api.model.series.Series;
+import com.axibase.tsd.api.model.sql.StringTable;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Igor Shmagrinskiy
+ */
+public class SqlSelectAllWithJoinTests extends SqlTest {
+    private static final String TEST_PREFIX = "sql-select-all-join-";
+    private static final String TEST_METRIC1_NAME = TEST_PREFIX + "metric-1";
+    private static final String TEST_METRIC2_NAME = TEST_PREFIX + "metric-2";
+    private static final String TEST_ENTITY = TEST_PREFIX + "entity-1";
+
+    @BeforeClass
+    public static void prepareData() {
+        Series leftSeries = new Series(TEST_ENTITY, TEST_METRIC1_NAME);
+        leftSeries.addTag("a", "b");
+        sendSamplesToSeries(leftSeries,
+                new Sample(Util.parseDate("2016-06-03T09:23:00.000Z").getTime(), "7"),
+                new Sample(Util.parseDate("2016-06-03T09:24:00.000Z").getTime(), "0"),
+                new Sample(Util.parseDate("2016-06-03T09:25:00.000Z").getTime(), "12"),
+                new Sample(Util.parseDate("2016-06-03T09:26:00.000Z").getTime(), "10.3"),
+                new Sample(Util.parseDate("2016-06-03T09:27:00.000Z").getTime(), "10")
+        );
+        leftSeries.setMetric(TEST_METRIC2_NAME);
+        leftSeries.addTag("b", "c");
+        Series rightSeries = leftSeries;
+        sendSamplesToSeries(rightSeries,
+                new Sample(Util.parseDate("2016-06-03T09:23:00.000Z").getTime(), "5"),
+                new Sample(Util.parseDate("2016-06-03T09:24:00.000Z").getTime(), "7"),
+                new Sample(Util.parseDate("2016-06-03T09:25:00.000Z").getTime(), "-2"),
+                new Sample(Util.parseDate("2016-06-03T09:26:00.000Z").getTime(), "-2.1")
+        );
+    }
+
+    /**
+     * Following tests related to issue #3033
+     */
+
+
+    /**
+     * issue #3033
+     */
+    @Test
+    public void testSelectAllColumnsWithAlias() {
+        String sqlQuery =
+                "SELECT * FROM 'sql-select-all-join-metric-1' t1 " +
+                        "JOIN 'sql-select-all-join-metric-2' t2";
+
+        StringTable resultTable = executeQuery(sqlQuery).readEntity(StringTable.class);
+
+        List<String> expectedColumnNames = Arrays.asList(
+                TEST_METRIC1_NAME + ".entity",
+                TEST_METRIC2_NAME + ".entity",
+                TEST_METRIC1_NAME + ".value",
+                TEST_METRIC2_NAME + ".value",
+                TEST_METRIC1_NAME + ".datetime",
+                TEST_METRIC2_NAME + ".datetime",
+                TEST_METRIC1_NAME + ".tags.a",
+                TEST_METRIC2_NAME + ".tags.a",
+                TEST_METRIC2_NAME + ".tags.b"
+        );
+
+        assertTableColumnsNames(expectedColumnNames, resultTable);
+    }
+
+
+    /**
+     * issue #3033
+     */
+    @Test
+    public void testSelectAllColumnsWithoutAlias() {
+        String sqlQuery =
+                "SELECT * FROM 'sql-select-all-join-metric-1' " +
+                        "JOIN 'sql-select-all-join-metric-2'";
+
+        StringTable resultTable = executeQuery(sqlQuery).readEntity(StringTable.class);
+
+        List<String> expectedColumnNames = Arrays.asList(
+                TEST_METRIC1_NAME + ".entity",
+                TEST_METRIC2_NAME + ".entity",
+                TEST_METRIC1_NAME + ".value",
+                TEST_METRIC2_NAME + ".value",
+                TEST_METRIC1_NAME + ".datetime",
+                TEST_METRIC2_NAME + ".datetime",
+                TEST_METRIC1_NAME + ".tags.a",
+                TEST_METRIC2_NAME + ".tags.a",
+                TEST_METRIC2_NAME + ".tags.b"
+        );
+
+        assertTableColumnsNames(expectedColumnNames, resultTable);
+    }
+
+
+    /**
+     * issue #3033
+     */
+    @Test
+    public void testSelectAllColumnsFromTableAlias() {
+        String sqlQuery =
+                "SELECT t1.* FROM 'sql-select-all-join-metric-1' t1 " +
+                        "JOIN 'sql-select-all-join-metric-2' t2";
+
+        StringTable resultTable = executeQuery(sqlQuery).readEntity(StringTable.class);
+
+        List<String> expectedColumnNames = Arrays.asList(
+                TEST_METRIC1_NAME + ".entity",
+                TEST_METRIC1_NAME + ".value",
+                TEST_METRIC1_NAME + ".datetime",
+                TEST_METRIC1_NAME + ".tags.a"
+        );
+
+        assertTableColumnsNames(expectedColumnNames, resultTable);
+    }
+
+}
