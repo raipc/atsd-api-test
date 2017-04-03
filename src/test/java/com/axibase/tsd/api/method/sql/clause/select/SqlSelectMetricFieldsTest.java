dfe5d263d2377ad2e7d4829c6da5566c61979ce2
alexey-kravtsov <Havok.08@mail.ru>
2017-04-03 12:28:47 +0300

    SQL: Add metric fields #3882 (#269)
    * Added test for "SQL: Add metric fields" #3882
    
    * Added metric.counter field test
    
    * Refactoring (added data provider)
    
    * Values comparison replaced with check on row existence
    
    * Added coverage for #3658
    
    * Removed test for id field

---
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/select/SqlSelectMetricFieldsTest.java
@@ -0,0 +1,63 @@
+package com.axibase.tsd.api.method.sql.clause.select;
+
+import com.axibase.tsd.api.method.series.SeriesMethod;
+import com.axibase.tsd.api.method.sql.SqlTest;
+import com.axibase.tsd.api.model.series.Series;
+import com.axibase.tsd.api.model.sql.StringTable;
+import com.axibase.tsd.api.util.Mocks;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class SqlSelectMetricFieldsTest extends SqlTest {
+
+    private static final Series TEST_SERIES = Mocks.series();
+
+    @BeforeClass
+    public static void prepareData() throws Exception {
+        SeriesMethod.insertSeriesCheck(TEST_SERIES);
+    }
+
+    @DataProvider(name = "metricFieldsProvider")
+    private Object[][] provideMetricFields() {
+        return new Object[][] {
+                {"name"},
+                {"label"},
+                {"timeZone"},
+                {"interpolate"},
+                {"description"},
+                {"dataType"},
+                {"timePrecision"},
+                {"enabled"},
+                {"persistent"},
+                {"filter"},
+                {"lastInsertTime"},
+                {"retentionIntervalDays"},
+                {"versioning"},
+                {"minValue"},
+                {"maxValue"},
+                {"invalidValueAction"},
+                {"counter"},
+                {"units"},
+                {"tags"}
+        };
+    }
+
+    /**
+     * #3882, #3658
+     */
+    @Test(dataProvider = "metricFieldsProvider")
+    public void testQueryMetricFields(String field) {
+        String sqlQuery = String.format(
+                "SELECT m.metric.%s FROM '%s' m",
+                field,
+                TEST_SERIES.getMetric());
+
+        StringTable resultTable = queryTable(sqlQuery);
+
+        // check for row existence
+        assertEquals(String.format("Error in metric field query (%s)", field), resultTable.getRows().size(), 1);
+    }
+}
