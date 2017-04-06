507ad124230f17599601aff17291a1cd6bff4ccf
Valery Tolstov <vtols@users.noreply.github.com>
2017-04-06 15:52:42 +0300

    SQL: sum function cannot be used in select expression (#276)
    * SQL: sum function cannot be used in select expression
    
    * Fix comment

---
+++ src/test/java/com/axibase/tsd/api/method/sql/function/aggregation/SumExpressionTest.java
@@ -0,0 +1,43 @@
+package com.axibase.tsd.api.method.sql.function.aggregation;
+
+import com.axibase.tsd.api.method.series.SeriesMethod;
+import com.axibase.tsd.api.method.sql.SqlTest;
+import com.axibase.tsd.api.model.series.Sample;
+import com.axibase.tsd.api.model.series.Series;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.axibase.tsd.api.util.TestUtil.TestNames.entity;
+import static com.axibase.tsd.api.util.TestUtil.TestNames.metric;
+
+public class SumExpressionTest extends SqlTest {
+
+    private static final String METRIC_NAME = metric();
+
+    @BeforeClass
+    public static void prepareData() throws Exception {
+        Series series = new Series(entity(), METRIC_NAME);
+
+        series.addData(new Sample("2017-01-01T12:00:00.000Z", "1"));
+        series.addData(new Sample("2017-01-02T12:00:00.000Z", "2"));
+        series.addData(new Sample("2017-01-03T12:00:00.000Z", "3"));
+        series.addData(new Sample("2017-01-04T12:00:00.000Z", "4"));
+        series.addData(new Sample("2017-01-05T12:00:00.000Z", "5"));
+
+        SeriesMethod.insertSeriesCheck(series);
+    }
+
+    /**
+     * #3950
+     */
+    @Test
+    public void testSumInExpression() {
+        String sqlQuery = String.format(
+                "SELECT sum(value * value) * sum(value * 2) FROM '%s'",
+                METRIC_NAME);
+
+        String[][] expectedRows = {{"1650"}};
+
+        assertSqlQueryRows("Wrong result for SUM aggregation function inside expressions", expectedRows, sqlQuery);
+    }
+}
