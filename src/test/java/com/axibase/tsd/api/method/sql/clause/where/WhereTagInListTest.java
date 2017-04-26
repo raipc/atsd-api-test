61d150149e0386ceb843648471317c95cd8251d6
Valery Tolstov <vtols@users.noreply.github.com>
2017-04-26 11:53:24 +0300

    SQL: NPE in WHERE clause IN list #4047 (#289)
    * SQL: NPE in WHERE clause IN list
    
    * Extract field and update message

---
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/where/WhereTagInListTest.java
@@ -0,0 +1,90 @@
+package com.axibase.tsd.api.method.sql.clause.where;
+
+import com.axibase.tsd.api.method.series.SeriesMethod;
+import com.axibase.tsd.api.method.sql.SqlTest;
+import com.axibase.tsd.api.model.series.Sample;
+import com.axibase.tsd.api.model.series.Series;
+import com.axibase.tsd.api.util.Registry;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.axibase.tsd.api.util.TestUtil.TestNames.entity;
+import static com.axibase.tsd.api.util.TestUtil.TestNames.metric;
+
+public class WhereTagInListTest extends SqlTest {
+    private static final String ENTITY_NAME = entity();
+    private static final String METRIC_NAME = metric();
+
+    @BeforeClass
+    public static void prepareData() throws Exception {
+        List<Series> seriesList = new ArrayList<>();
+        String[][] tags = {
+                {"t1", "a"},
+                {"t1", "b"},
+                {"t1", "a"},
+                {"t2", "c"},
+                {"t1", "d"},
+                {null, null},
+        };
+
+        Registry.Entity.register(ENTITY_NAME);
+        Registry.Metric.register(METRIC_NAME);
+
+        for (int i = 0; i < tags.length; i++) {
+            Series series = new Series();
+            series.setEntity(ENTITY_NAME);
+            series.setMetric(METRIC_NAME);
+            series.addData(new Sample(String.format("2017-01-0%dT12:00:00.000Z", i + 1),
+                    Integer.toString(i + 1)));
+
+            if (tags[i][0] != null) {
+                series.addTag(tags[i][0], tags[i][1]);
+            }
+
+            seriesList.add(series);
+        }
+
+        SeriesMethod.insertSeriesCheck(seriesList);
+    }
+
+    /**
+     * #4047
+     */
+    @Test
+    public void testWhereTagInList() {
+        String sqlQuery = String.format(
+                "SELECT value " +
+                        "FROM '%s' " +
+                        "WHERE tags.t1 IN ('a', 'b')",
+                METRIC_NAME
+        );
+
+        String[][] expectedRows = {
+                {"1"}, {"2"}, {"3"}
+        };
+
+        assertSqlQueryRows("Wrong result with WHERE <tag> IN <list>", expectedRows, sqlQuery);
+    }
+
+    /**
+     * #4047
+     */
+    @Test
+    public void testWhereTagNotInList() {
+        String sqlQuery = String.format(
+                "SELECT value " +
+                        "FROM '%s' " +
+                        "WHERE tags.t1 NOT IN ('a', 'b')",
+                METRIC_NAME
+        );
+
+        String[][] expectedRows = {
+                {"5"}
+        };
+
+        assertSqlQueryRows("Wrong result with WHERE <tag> NOT IN <list>", expectedRows, sqlQuery);
+    }
+}
