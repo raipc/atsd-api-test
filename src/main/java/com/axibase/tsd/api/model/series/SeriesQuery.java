245fb7763ea9409921c88a5202c190970d710bf0
alexey-kravtsov <Havok.08@mail.ru>
2017-11-08 09:24:49 +0300

    Data API: query limit with ascending order #4635 (#437)
    * added test for #4635
    
    * added test for default order and descriptions
    
    * fixed query dates

--- src/main/java/com/axibase/tsd/api/model/series/SeriesQuery.java
+++ src/main/java/com/axibase/tsd/api/model/series/SeriesQuery.java
@@ -3,6 +3,7 @@ package com.axibase.tsd.api.model.series;
 import com.axibase.tsd.api.model.Interval;
 import com.axibase.tsd.api.util.Util;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
 
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.Map;
 import static com.axibase.tsd.api.util.Util.MAX_QUERYABLE_DATE;
 import static com.axibase.tsd.api.util.Util.MIN_QUERYABLE_DATE;
 
-
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SeriesQuery {
     private String entity;
@@ -29,6 +30,8 @@ public class SeriesQuery {
     private String timeFormat;
     private Boolean exactMatch;
     private Integer limit;
+    private Boolean cache;
+    private String direction;
     private Integer seriesLimit;
     private Boolean versioned;
 
@@ -54,11 +57,11 @@ public class SeriesQuery {
     }
 
     public SeriesQuery(String entity, String metric, long startTime, long endTime) {
-        this(entity, metric, Util.ISOFormat(startTime), Util.ISOFormat(endTime), new HashMap<String, String>());
+        this(entity, metric, Util.ISOFormat(startTime), Util.ISOFormat(endTime), new HashMap<>());
     }
 
     public SeriesQuery(String entity, String metric, String startDate, String endDate) {
-        this(entity, metric, startDate, endDate, new HashMap<String, String>());
+        this(entity, metric, startDate, endDate, new HashMap<>());
     }
 
     public SeriesQuery(String entity, String metric, String startDate, String endDate, Map<String, String> tags) {
@@ -67,6 +70,10 @@ public class SeriesQuery {
         this.startDate = startDate;
         this.endDate = endDate;
         this.tags = tags;
+    }
+
+    public void addTags(String tag, String value) {
+        tags.put(tag, value);
     }
 
     private void setIntervalBasedOnSeriesDate(final Series series) throws IllegalArgumentException {
@@ -87,149 +94,8 @@ public class SeriesQuery {
         setEndDate(Util.ISOFormat(maxDate + 1));
     }
 
-    public Integer getLimit() {
-        return limit;
-    }
-
-    public void setLimit(Integer limit) {
-        this.limit = limit;
-    }
-
-    public Integer getSeriesLimit() {
-        return seriesLimit;
-    }
-
-    public void setSeriesLimit(Integer seriesLimit) {
-        this.seriesLimit = seriesLimit;
-    }
-
-    public Group getGroup() {
-        return group;
-    }
-
-    public void setGroup(Group group) {
-        this.group = group;
-    }
-
-    public List<String> getEntities() {
-        return entities;
-    }
-
-    public void setEntities(List<String> entities) {
-        this.entities = entities;
-    }
-
-    public Aggregate getAggregate() {
-        return aggregate;
-    }
-
-    public void setAggregate(Aggregate aggregate) {
-        this.aggregate = aggregate;
-    }
-
-    public String getEntity() {
-        return entity;
-    }
-
-    public void setEntity(String entity) {
-        this.entity = entity;
-    }
-
-    public String getMetric() {
-        return metric;
-    }
-
-    public void setMetric(String metric) {
-        this.metric = metric;
-    }
-
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
-    public Map<String, String> getTags() {
-        return tags;
-    }
-
-    public void setTags(Map<String, String> tags) {
-        this.tags = tags;
-    }
-
-    public void addTags(String tag, String value) {
-        tags.put(tag, value);
-    }
-
     @Override
     public String toString() {
         return Util.prettyPrint(this);
-    }
-
-    public String getTimeFormat() {
-        return timeFormat;
-    }
-
-    public void setTimeFormat(String timeFormat) {
-        this.timeFormat = timeFormat;
-    }
-
-    public Interval getInterval() {
-        return interval;
-    }
-
-    public void setInterval(Interval interval) {
-        this.interval = interval;
-    }
-
-    public Boolean getExactMatch() {
-        return exactMatch;
-    }
-
-    public void setExactMatch(Boolean exactMatch) {
-        this.exactMatch = exactMatch;
-    }
-
-    public String getEntityExpression() {
-        return entityExpression;
-    }
-
-    public void setEntityExpression(String entityExpression) {
-        this.entityExpression = entityExpression;
-    }
-
-    public String getEntityGroup() {
-        return entityGroup;
-    }
-
-    public void setEntityGroup(String entityGroup) {
-        this.entityGroup = entityGroup;
-    }
-
-    public Boolean getVersioned() {
-        return versioned;
-    }
-
-    public void setVersioned(Boolean versioned) {
-        this.versioned = versioned;
-    }
-
-    public String getTagExpression() {
-        return tagExpression;
-    }
-
-    public void setTagExpression(String tagExpression) {
-        this.tagExpression = tagExpression;
     }
 }
