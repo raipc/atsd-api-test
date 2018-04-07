8788688adbdfbf2f043078ebb5a32cef1ade7f86
unrealwork <unrealwork@gmail.com>
2018-04-07 20:17:49 +0300

    new checker

--- src/main/java/com/axibase/tsd/api/Checker.java
+++ src/main/java/com/axibase/tsd/api/Checker.java
@@ -4,9 +4,17 @@ package com.axibase.tsd.api;
 import com.axibase.tsd.api.method.BaseMethod;
 import com.axibase.tsd.api.method.checks.AbstractCheck;
 import com.axibase.tsd.api.util.NotCheckedException;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 public class Checker {
+
     public static void check(AbstractCheck check) {
+        new Checker().verify(check);
+    }
+
+    public void verify(AbstractCheck check) {
         Long startTime = System.currentTimeMillis();
         while (!check.isChecked()) {
             if (System.currentTimeMillis() - BaseMethod.UPPER_BOUND_FOR_CHECK > startTime) {
@@ -15,6 +23,7 @@ public class Checker {
                 try {
                     Thread.sleep(BaseMethod.REQUEST_INTERVAL);
                 } catch (InterruptedException e) {
+                    log.error("Check sleep was interrupted! {}", e.getCause());
                     throw new IllegalStateException(e.getMessage());
                 }
             }
