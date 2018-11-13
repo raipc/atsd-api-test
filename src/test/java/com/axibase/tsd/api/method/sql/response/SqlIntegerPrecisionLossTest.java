c2c2eccd337f0c9d0a3e60cbb0dc5d17ef9deef1
sergei <gray4747@gmail.com>
2018-11-13 19:15:52 +0300

    Tests on numbers output created

---
+++ src/test/java/com/axibase/tsd/api/method/sql/response/SqlIntegerPrecisionLossTest.java
@@ -0,0 +1,87 @@
+package com.axibase.tsd.api.method.sql.response;
+
+import com.axibase.tsd.api.method.series.SeriesMethod;
+import com.axibase.tsd.api.method.sql.SqlTest;
+import com.axibase.tsd.api.model.series.Sample;
+import com.axibase.tsd.api.model.series.Series;
+import com.axibase.tsd.api.model.sql.StringTable;
+import io.qameta.allure.Issue;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+public class SqlIntegerPrecisionLossTest extends SqlTest {
+
+
+    private static final String TEST_PREFIX = "integer-precision-";
+    private static final String TEST_ENTITY_NAME = TEST_PREFIX + "entity";
+    private static final String TEST_METRIC_NAME_POS = TEST_PREFIX + "posmetric";
+    private static final String TEST_METRIC_NAME_NEG = TEST_PREFIX + "negmetric";
+
+    @BeforeClass
+    public void prepareData() throws Exception {
+        Series negSeries = createTestSeries(false);
+        Series PosSeries = createTestSeries(true);
+
+        SeriesMethod.insertSeriesCheck(negSeries);
+        SeriesMethod.insertSeriesCheck(PosSeries);
+    }
+
+    private static Series createTestSeries(boolean isPositive) {
+        long time = 1541030400000L, value;
+        Series series;
+        if (isPositive) {
+            series = new Series(TEST_ENTITY_NAME, TEST_METRIC_NAME_POS);
+            series.addSamples(Sample.ofTimeDecimal(time++, new BigDecimal(0x7FFFFFFFFFFFFFFFL)));
+            value = 1111111111111111111L;
+        } else {
+            series = new Series(TEST_ENTITY_NAME, TEST_METRIC_NAME_NEG);
+            series.addSamples(Sample.ofTimeDecimal(time++, new BigDecimal(0x8000000000000000L)));
+            value = -1111111111111111111L;
+        }
+
+        while (value != 0) {
+            series.addSamples(Sample.ofTimeDecimal(time++, new BigDecimal(value)));
+            value /= 10;
+        }
+
+        return series;
+    }
+
+    @Issue("5736")
+    @Test
+    public void testPositiveValues() {
+        String query = String.format("SELECT value from \"%s\" where entity='%s'", TEST_METRIC_NAME_POS, TEST_ENTITY_NAME);
+
+        String[][] expectedRows = new String[20][1];
+        expectedRows[0][0] = Long.toString(Long.MAX_VALUE);
+        long value = 1111111111111111111L;
+        for (int i = 1; i < 20; i++) {
+            expectedRows[i][0] = Long.toString(value);
+            value /= 10;
+        }
+        StringTable resultTable = queryResponse(query).readEntity(StringTable.class);
+
+
+        assertRowsMatch("Wrong table content", expectedRows, resultTable, query);
+    }
+
+    @Issue("5736")
+    @Test
+    public void testNegativeValues() {
+        String query = String.format("SELECT value from \"%s\" where entity='%s'", TEST_METRIC_NAME_NEG, TEST_ENTITY_NAME);
+
+        String[][] expectedRows = new String[20][1];
+        expectedRows[0][0] = Long.toString(Long.MIN_VALUE);
+        long value = -1111111111111111111L;
+        for (int i = 1; i < 20; i++) {
+            expectedRows[i][0] = Long.toString(value);
+            value /= 10;
+        }
+        StringTable resultTable = queryResponse(query).readEntity(StringTable.class);
+
+
+        assertRowsMatch("Wrong table content", expectedRows, resultTable, query);
+    }
+}
