d1b3a86558664f3a4cc7770e9edb41e843b53829
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-05-31 12:01:51 +0300

    Added Empty tag test for tcp transport

--- src/test/java/com/axibase/tsd/api/method/property/transport/tcp/EmptyTagTest.java
+++ src/test/java/com/axibase/tsd/api/method/property/transport/tcp/EmptyTagTest.java
@@ -1,12 +1,10 @@
-package com.axibase.tsd.api.method.property.command;
+package com.axibase.tsd.api.method.property.transport.tcp;
 
-
-import com.axibase.tsd.api.method.extended.CommandMethod;
 import com.axibase.tsd.api.method.property.PropertyMethod;
 import com.axibase.tsd.api.model.command.PlainCommand;
 import com.axibase.tsd.api.model.command.PropertyCommand;
 import com.axibase.tsd.api.model.property.Property;
-import com.axibase.tsd.api.model.extended.CommandSendingResult;
+import com.axibase.tsd.api.transport.tcp.TCPSender;
 import io.qameta.allure.Issue;
 import org.testng.annotations.Test;
 
@@ -23,38 +21,38 @@ public class EmptyTagTest extends PropertyMethod {
 
     @Issue("6234")
     @Test
-    public void emptyTagFailTest() {
-        Property property = new Property("property-command-emptytag-test-t1", "property-command-emptytag-test-e1");
+    public void emptyTagFailTest() throws Exception {
+        Property property = new Property("property-tcp-emptytag-test-t1", "property-tcp-emptytag-test-e1");
         Map<String, String> tagMap = new HashMap<>();
         tagMap.put("t1", "\r");
         property.setTags(tagMap);
         property.setDate(getCurrentDate());
         PlainCommand command = new PropertyCommand(property);
-        CommandSendingResult result = CommandMethod.send(command);
+        String result = TCPSender.send(command, true);
         tagMap.remove("t1");
         property.setTags(tagMap);
-        assertTrue("The only command had to fail. Actual response: " + result.toString(), result.getFail() == 1 && result.getTotal() == 1);
+        assertTrue(command.compose() + " had to fail, but did not.", result.startsWith("Empty tag values in command"));
     }
 
     @Issue("6234")
     @Test
-    public void emptyAndNonEmptyTagTest() {
-        Property property = new Property("property-command-emptytag-test-t2", "property-command-emptytag-test-e2");
+    public void emptyAndNonEmptyTagTest() throws Exception{
+        Property property = new Property("property-tcp-emptytag-test-t2", "property-tcp-emptytag-test-e2");
         Map<String, String> tagMap = new HashMap<>();
         tagMap.put("t1", "v1");
         tagMap.put("t2", "v2");
         property.setTags(tagMap);
         property.setDate(getCurrentDate());
         PlainCommand nonEmptyCommand = new PropertyCommand(property);
-        CommandMethod.send(nonEmptyCommand);
+        TCPSender.send(nonEmptyCommand, true);
+        //CommandMethod.send(nonEmptyCommand);
 
         Map<String, String> emptyTagMap = new HashMap<>();
         emptyTagMap.put("t1", "v1-new");
         emptyTagMap.put("t2", "\r");
         property.setTags(emptyTagMap);
         PlainCommand emptyTagCommand = new PropertyCommand(property);
-        CommandSendingResult result = CommandMethod.send(emptyTagCommand);
-        assertTrue("The only command had to succeed. Actual response: " + result.toString(), result.getSuccess() == 1 && result.getTotal() == 1);
+        TCPSender.send(emptyTagCommand, true);
 
         emptyTagMap.remove("t2");
         property.setTags(emptyTagMap);
@@ -63,8 +61,8 @@ public class EmptyTagTest extends PropertyMethod {
 
     @Issue("6234")
     @Test
-    public void keyAndEmptyTagTest() {
-        Property property = new Property("property-command-emptytag-test-t3", "property-command-emptytag-test-e3");
+    public void keyAndEmptyTagTest() throws Exception {
+        Property property = new Property("property-tcp-emptytag-test-t3", "property-tcp-emptytag-test-e3");
         Map<String, String> keyMap = new HashMap<>();
         keyMap.put("k1", "vk1");
         property.setKey(keyMap);
@@ -73,9 +71,9 @@ public class EmptyTagTest extends PropertyMethod {
         property.setTags(tagMap);
         property.setDate(getCurrentDate());
         PlainCommand command = new PropertyCommand(property);
-        CommandSendingResult result = CommandMethod.send(command);
+        String result = TCPSender.send(command, true);
         tagMap.remove("t1");
         property.setTags(tagMap);
-        assertTrue("The only command had to fail. Actual response: " + result.toString(), result.getFail() == 1 && result.getTotal() == 1);
+        assertTrue(command.compose() + " had to fail, but did not.", result.startsWith("Empty tag values in command"));
     }
 }
