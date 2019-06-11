9d00c7223aaa1bb0bd9ba0d1c5e7b9b582d85cf8
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-06-11 13:32:41 +0300

    Changed empty tags to data providers

--- src/test/java/com/axibase/tsd/api/method/property/command/EmptyTagTest.java
+++ src/test/java/com/axibase/tsd/api/method/property/command/EmptyTagTest.java
@@ -10,6 +10,7 @@ import com.axibase.tsd.api.util.Mocks;
 import com.google.common.collect.ImmutableMap;
 import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -17,74 +18,85 @@ import java.util.Map;
 
 import static com.axibase.tsd.api.method.property.PropertyTest.assertPropertyExisting;
 import static com.axibase.tsd.api.util.TestUtil.getCurrentDate;
-import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.*;
 
 public class EmptyTagTest extends PropertyMethod {
 
+    @DataProvider
+    private Object[][] emptyValues() {
+        return new String[][] {
+                {""},
+                {"\r"},
+                {"\n"},
+                {"\r\n"},
+                {" "},
+                {"  "},
+                {" \n "},
+                {" \n"},
+                {"\n "}
+        };
+    }
+
     @Issue("6234")
     @Description("Tests that if the only tag in insertion command is empty, it fails.")
-    @Test
-    public void emptyTagFailTest() {
+    @Test(
+            dataProvider = "emptyValues"
+    )
+    public void emptyTagFailTest(String emptyValue) {
         String propertyType = Mocks.propertyType();
         String entityName = Mocks.entity();
         Property property = new Property(propertyType, entityName);
-        Map<String, String> tagMap = ImmutableMap.<String, String>builder()
-                .put("t1", "\r")
-                .build();
+        Map<String, String> tagMap = ImmutableMap.of("t1", emptyValue);
         property.setTags(tagMap);
         PlainCommand command = new PropertyCommand(property);
         CommandSendingResult result = CommandMethod.send(command);
-        assertTrue("The only command had to fail. Actual response: " + result.toString(), result.getFail() == 1);
-        assertTrue("Total commands count is not 1: " + result.toString(), result.getTotal() == 1);
+        assertEquals("The only command had to fail. Actual response: " + result.toString(), result.getFail(), Integer.valueOf(1));
+        assertEquals("Total commands count is not 1: " + result.toString(), result.getTotal(), Integer.valueOf(1));
     }
 
     @Issue("6234")
     @Description("Tests that if one tag is empty and one is not, property is inserted and tag with empty value is deleted.")
-    @Test
-    public void emptyAndNonEmptyTagTest() {
+    @Test(
+            dataProvider = "emptyValues"
+    )
+    public void emptyAndNonEmptyTagTest(String emptyValue) {
         String propertyType = Mocks.propertyType();
         String entityName = Mocks.entity();
         Property property = new Property(propertyType, entityName);
-        Map<String, String> tagMap = ImmutableMap.<String, String>builder()
-                .put("t1", "v1")
-                .put("t2", "v2")
-                .build();
+        Map<String, String> tagMap = ImmutableMap.of("t1", "v1", "t2", "v2");
         property.setTags(tagMap);
         PlainCommand nonEmptyCommand = new PropertyCommand(property);
         CommandMethod.send(nonEmptyCommand);
 
-        Map<String, String> emptyTagMap = ImmutableMap.<String, String>builder()
-                .put("t1","v1-new")
-                .put("t2", "\r")
-                .build();
+        Map<String, String> emptyTagMap = ImmutableMap.of("t1", "v1-new", "t2", emptyValue);
         property.setTags(emptyTagMap);
         PlainCommand emptyTagCommand = new PropertyCommand(property);
         CommandSendingResult result = CommandMethod.send(emptyTagCommand);
-        assertTrue("The only command had to succeed. Actual response: " + result.toString(), result.getSuccess() == 1);
-        assertTrue("Total commands count is not 1: " + result.toString(), result.getTotal() == 1);
+        assertEquals("The only command had to succeed. Actual response: " + result.toString(), result.getSuccess(), Integer.valueOf( 1));
+        assertEquals("Total commands count is not 1: " + result.toString(), result.getTotal(), Integer.valueOf( 1));
 
-        property.setTags(ImmutableMap.<String, String>builder().put("t1", "v1-new").build());
+        property.setTags(ImmutableMap.of("t1", "v1-new"));
         assertPropertyExisting(property);
     }
 
     @Issue("6234")
     @Description("Tests that if key is not empty and the only tg is empty, property is not inserted.")
-    @Test
-    public void keyAndEmptyTagTest() {
+    @Test(
+            dataProvider = "emptyValues"
+    )
+    public void keyAndEmptyTagTest(String emptyValue) {
         String propertyType = Mocks.propertyType();
         String entityName = Mocks.entity();
         Property property = new Property(propertyType, entityName);
         Map<String, String> keyMap = new HashMap<>();
         keyMap.put("k1", "vk1");
         property.setKey(keyMap);
-        Map<String, String> tagMap = ImmutableMap.<String, String>builder()
-                .put("t1", "\r")
-                .build();
+        Map<String, String> tagMap = ImmutableMap.of("t1", emptyValue);
         property.setTags(tagMap);
         property.setDate(getCurrentDate());
         PlainCommand command = new PropertyCommand(property);
         CommandSendingResult result = CommandMethod.send(command);
-        assertTrue("The only command had to fail. Actual response: " + result.toString(), result.getFail() == 1);
-        assertTrue("Total commands count is not 1: " + result.toString(), result.getTotal() == 1);
+        assertEquals("The only command had to fail. Actual response: " + result.toString(), result.getFail(), Integer.valueOf(1));
+        assertEquals("Total commands count is not 1: " + result.toString(), result.getTotal(), Integer.valueOf(1));
     }
 }
--- src/test/java/com/axibase/tsd/api/method/property/transport/tcp/EmptyTagTest.java
+++ src/test/java/com/axibase/tsd/api/method/property/transport/tcp/EmptyTagTest.java
@@ -1,6 +1,5 @@
 package com.axibase.tsd.api.method.property.transport.tcp;
 
-import com.axibase.tsd.api.method.checks.PropertyCheck;
 import com.axibase.tsd.api.method.property.PropertyMethod;
 import com.axibase.tsd.api.model.command.PlainCommand;
 import com.axibase.tsd.api.model.command.PropertyCommand;
@@ -10,6 +9,7 @@ import com.axibase.tsd.api.util.Mocks;
 import com.google.common.collect.ImmutableMap;
 import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -21,16 +21,31 @@ import static org.testng.AssertJUnit.assertTrue;
 
 public class EmptyTagTest extends PropertyMethod {
 
+    @DataProvider
+    private Object[][] emptyValues() {
+        return new String[][] {
+                {""},
+                {"\r"},
+                {"\n"},
+                {"\r\n"},
+                {" "},
+                {"  "},
+                {" \n "},
+                {" \n"},
+                {"\n "}
+        };
+    }
+
     @Issue("6234")
     @Description("Tests that if the only tag in insertion command is empty, it fails.")
-    @Test
-    public void emptyTagFailTest() throws Exception {
+    @Test(
+            dataProvider = "emptyValues"
+    )
+    public void emptyTagFailTest(String emptyValue) throws Exception {
         String propertyType = Mocks.propertyType();
         String entityName = Mocks.entity();
         Property property = new Property(propertyType, entityName);
-        Map<String, String> tagMap = ImmutableMap.<String, String>builder()
-                .put("t1", "\r")
-                .build();
+        Map<String, String> tagMap = ImmutableMap.of("t1", emptyValue);
         property.setTags(tagMap);
         PlainCommand command = new PropertyCommand(property);
         String result = TCPSender.send(command, true);
@@ -39,24 +54,20 @@ public class EmptyTagTest extends PropertyMethod {
 
     @Issue("6234")
     @Description("Tests that if one tag is empty and one is not, property is inserted and tag with empty value is deleted.")
-    @Test
-    public void emptyAndNonEmptyTagTest() throws Exception {
+    @Test(
+            dataProvider = "emptyValues"
+    )
+    public void emptyAndNonEmptyTagTest(String emptyValue) throws Exception {
         String propertyType = Mocks.propertyType();
         String entityName = Mocks.entity();
         Property property = new Property(propertyType, entityName);
-        Map<String, String> tagMap = ImmutableMap.<String, String>builder()
-                .put("t1", "v1")
-                .put("t2", "v2")
-                .build();
+        Map<String, String> tagMap = ImmutableMap.of("t1", "v1", "t2", "v2");
         property.setTags(tagMap);
         property.setDate(getCurrentDate());
         PlainCommand nonEmptyCommand = new PropertyCommand(property);
         TCPSender.send( nonEmptyCommand, true);
 
-        Map<String, String> emptyTagMap = ImmutableMap.<String, String>builder()
-                .put("t1","v1-new")
-                .put("t2", "\r")
-                .build();
+        Map<String, String> emptyTagMap = ImmutableMap.of("t1", "v1-new", "t2", emptyValue);
         property.setTags(emptyTagMap);
         PlainCommand emptyTagCommand = new PropertyCommand(property);
         TCPSender.send(emptyTagCommand, true);
@@ -67,17 +78,17 @@ public class EmptyTagTest extends PropertyMethod {
 
     @Issue("6234")
     @Description("Tests that if key is not empty and the only tg is empty, property is not inserted.")
-    @Test
-    public void keyAndEmptyTagTest() throws Exception {
+    @Test(
+            dataProvider = "emptyValues"
+    )
+    public void keyAndEmptyTagTest(String emptyValue) throws Exception {
         String propertyType = Mocks.propertyType();
         String entityName = Mocks.entity();
         Property property = new Property(propertyType, entityName);
         Map<String, String> keyMap = new HashMap<>();
         keyMap.put("k1", "vk1");
         property.setKey(keyMap);
-        Map<String, String> tagMap = ImmutableMap.<String, String>builder()
-                .put("t1", "\r")
-                .build();
+        Map<String, String> tagMap = ImmutableMap.of("t1", emptyValue);
         property.setTags(tagMap);
         PlainCommand command = new PropertyCommand(property);
         String result = TCPSender.send(command, true);
