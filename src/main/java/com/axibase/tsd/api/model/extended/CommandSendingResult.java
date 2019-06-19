9383ac16737db585c803d682e6bbd726dbcf21ab
Anton Rybochkin <anton.rybochkin@axibase.com>
2019-06-19 17:23:33 +0300

    added all args constructor to be used by Jackson

--- src/main/java/com/axibase/tsd/api/model/extended/CommandSendingResult.java
+++ src/main/java/com/axibase/tsd/api/model/extended/CommandSendingResult.java
@@ -2,18 +2,18 @@ package com.axibase.tsd.api.model.extended;
 
 
 import com.axibase.tsd.api.util.Util;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CommandSendingResult {
-    private int fail;
-    private int success;
-    private int total;
+    private final int fail;
+    private final int success;
+    private final int total;
 
     public CommandSendingResult(int fail, int success) {
-        this.fail = fail;
-        this.success = success;
-        this.total = fail + success;
+        this(fail, success, fail + success);
     }
 
     @Override
