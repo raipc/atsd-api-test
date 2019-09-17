b5cd3c22dc554dc0465953f3bd25722b62aa7995
Anton Rybochkin <anton.rybochkin@axibase.com>
2019-09-17 14:54:07 +0300

    Expected error fixed in SQL OFFSET test (#587)

--- src/test/java/com/axibase/tsd/api/method/sql/clause/limit/SqlClauseLimitOffsetTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/clause/limit/SqlClauseLimitOffsetTest.java
@@ -186,7 +186,7 @@ public class SqlClauseLimitOffsetTest extends SqlTest {
 
     @Issue("3229")
     @Test
-    public void testInCorrectOffsetWithoutLimit() {
+    public void testIncorrectOffsetWithoutLimit() {
         String sqlQuery = String.format(
                 "SELECT value FROM \"%s\" %nOFFSET 1",
                 TEST_METRIC_NAME
@@ -197,7 +197,7 @@ public class SqlClauseLimitOffsetTest extends SqlTest {
         String expectedErrorMessage = ErrorTemplate.Sql.syntaxError(2, 0,
                 extraneousErrorMessage("OFFSET", "{<EOF>, INTEGER_LITERAL, ID, WORD, METRIC_NAME," +
                         " STRING_LITERAL, DQ_STRING_LITERAL, WHERE, AS, ORDER, GROUP, LIMIT, WITH, INNER, OUTER," +
-                        " FULL, JOIN, LOOKUP, OPTION}"));
+                        " FULL, JOIN, OPTION}"));
         assertBadRequest(
                 DEFAULT_ASSERT_MESSAGE,
                 expectedErrorMessage, response
