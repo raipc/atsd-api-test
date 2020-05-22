dc3351b62e00d3828ffb0c4fcf31e43adc29572f
Aleksandr Veselov <alexander.veselov@axibase.com>
2020-05-22 06:23:17 +0300

    Add tests for PR tsd#206 (#608)
    * Add tests for PR tsd#206
    
    * Replace cyrillic example name
    
    * Add test for replacement of IShares acronyms
    
    * Fix test issue with ishare's test case

--- src/test/java/com/axibase/tsd/api/method/financial/InstrumentSearchTest.java
+++ src/test/java/com/axibase/tsd/api/method/financial/InstrumentSearchTest.java
@@ -17,9 +17,12 @@ public class InstrumentSearchTest extends InstrumentSearchBase {
                 .trade("share2", "TQBR", "Primary Share Two")
                 .tradeUnassigned("share4", "EQOB", "Unassigned Share Four")
                 .tradeUnassigned("share5", "TQOB", "Unassigned Share Five")
-                .trade("descpfx", "TEST", "Shar_es4Test Inc.")
+                .trade("descpfx", "TEST", "SharAEEEes4Test Inc.")
                 .trade("option1", "TEST", "First Option")
                 .trade("option2", "TEST", "Second Option")
+                .trade("PRIM", "TEST", "ПАО 'Пример'")
+                .trade("PRIS", "TEST", "ОАО Пример и Примерчики")
+                .trade("TEST_ETF", "TEST", "THE ISH TEST")
                 .insert()
                 .waitUntilTradesInsertedAtMost(5, TimeUnit.MINUTES);
     }
@@ -41,10 +44,28 @@ public class InstrumentSearchTest extends InstrumentSearchBase {
     }
 
     @Test
+    public void testFoundCyrillic() {
+        searchAndTest("Прим",
+                entry("PRIM", "TEST", "ПАО 'Пример'"),
+                entry("PRIS", "TEST", "ОАО Пример и Примерчики")
+        );
+        searchAndTest("ПримерИ",
+                entry("PRIS", "TEST", "ОАО Пример и Примерчики")
+        );
+    }
+
+    @Test
     public void testFoundByClassName() {
         searchAndTest("TQbR",
                 entry("share2", "TQBR", "Primary Share Two"),
                 entry("share3", "TQBR", "Primary Share")
+        );
+    }
+
+    @Test
+    public void testAcronymReplacement() {
+        searchAndTest("ishares",
+                entry("TEST_ETF", "TEST", "THE ISH TEST")
         );
     }
 
@@ -59,7 +80,8 @@ public class InstrumentSearchTest extends InstrumentSearchBase {
                 entry("share1", "TEST", "First Share"),
                 entry("share10", "TEST", ""),
                 entry("share2", "TEST", "Second Share"),
-                entryUnassigned("share4", "EQOB", "Unassigned Share Four")
+                entryUnassigned("share4", "EQOB", "Unassigned Share Four"),
+                entry("TEST_ETF", "TEST", "THE ISH TEST")
         );
     }
 
@@ -78,7 +100,8 @@ public class InstrumentSearchTest extends InstrumentSearchBase {
                 entry("share10", "TEST", ""),
                 entry("share2", "TEST", "Second Share"),
                 entryUnassigned("share4", "EQOB", "Unassigned Share Four"),
-                entry("descpfx", "TEST", "Shar_es4Test Inc.")
+                entry("descpfx", "TEST", "SharAEEEes4Test Inc."),
+                entry("TEST_ETF", "TEST", "THE ISH TEST")
         );
     }
 
@@ -92,21 +115,23 @@ public class InstrumentSearchTest extends InstrumentSearchBase {
     }
 
     @Test
-    public void testFoundBySubstring() { searchAndTest("hARe",
+    public void testFoundBySubstring() {
+        searchAndTest("hARe",
                 entry("share2", "TQBR", "Primary Share Two"),
                 entry("share3", "TQBR", "Primary Share"),
                 entryUnassigned("share5", "TQOB", "Unassigned Share Five"),
                 entry("share1", "TEST", "First Share"),
                 entry("share10", "TEST", ""),
                 entry("share2", "TEST", "Second Share"),
-                entryUnassigned("share4", "EQOB", "Unassigned Share Four")
+                entryUnassigned("share4", "EQOB", "Unassigned Share Four"),
+                entry("TEST_ETF", "TEST", "THE ISH TEST")
         );
         searchAndTest("irst",
                 entry("option1", "TEST", "First Option"),
                 entry("share1", "TEST", "First Share")
         );
         searchAndTest("4Test",
-                entry("descpfx", "TEST", "Shar_es4Test Inc.")
+                entry("descpfx", "TEST", "SharAEEEes4Test Inc.")
         );
     }
 }
