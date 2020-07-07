298668f5b597a84db8f79e754bdf3ebf5c42a65f
Alexander Tokarev <atokarev@gmail.com>
2020-07-07 14:01:47 +0300

    Tests for is_weekday function and negation expressions (#629)

--- src/test/java/com/axibase/tsd/api/method/sql/trade/TradeIsWorkdayTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/trade/TradeIsWorkdayTest.java
@@ -61,7 +61,25 @@ public class TradeIsWorkdayTest extends SqlTradeTest {
                 test("Test saturday")
                         .sql("select datetime from atsd_trade where {instrument} and {time} and is_workday(datetime) WITH TIMEZONE = 'Europe/Moscow', WORKDAY_CALENDAR = 'usa'")
                         .time(" time between '2018-12-28T00:00:00Z' and '2018-12-30T00:00:00Z' ")
-                        .addExpected("2018-12-28T10:00:00.000000Z")
+                        .addExpected("2018-12-28T10:00:00.000000Z"),
+
+                test("Test is_weekday function")
+                        .sql("select datetime from atsd_trade where {instrument} and {time} and is_weekday(time, 'usa', 'UTC') WITH TIMEZONE = 'Europe/Moscow', WORKDAY_CALENDAR = 'rus'")
+                        .addExpected("2020-04-16T23:00:00.000000Z")
+                        .addExpected("2020-04-17T23:00:00.000000Z"),
+
+                test("Test is_weekday function negate")
+                        .sql("select datetime from atsd_trade where {instrument} and {time} and not is_weekday(time, 'usa', 'UTC') WITH TIMEZONE = 'Europe/Moscow', WORKDAY_CALENDAR = 'rus'")
+                        .addExpected("2020-04-18T23:00:00.000000Z"),
+
+                test("Test is_weekday function negate aggregation")
+                        .sql("select datetime, open() from atsd_trade where {instrument} and {time} and not is_weekday(time, 'usa', 'UTC') group by exchange, class, symbol, period(1 day, 'UTC')")
+                        .addExpected("2020-04-18T00:00:00.000000Z", "1"),
+
+                test("Test is_workday function negate aggregation")
+                        .sql("select datetime, open() from atsd_trade where {instrument} and {time} and not is_workday(time, 'usa', 'UTC') group by exchange, class, symbol, period(1 day, 'UTC')")
+                        .addExpected("2020-04-16T00:00:00.000000Z", "2")
+                        .addExpected("2020-04-18T00:00:00.000000Z", "1"),
 
         };
         return TestUtil.convertTo2DimArray(data);
