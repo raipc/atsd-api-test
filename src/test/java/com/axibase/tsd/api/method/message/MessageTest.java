e0c4f7d65dd074b9f67a6d9307a7448e5f1d71ed
Mikhail Zvagelsky <mikhail.zvagelsky@axibase.com>
2021-02-25 16:35:32 +0300

    API message tests: check that message entity is created before check the message itself. (#683)
    * API message tests: check that message entity is created before check the message itself.
    
    * Remove sout.
    
    * Fix spelling.

--- src/test/java/com/axibase/tsd/api/method/message/MessageTest.java
+++ src/test/java/com/axibase/tsd/api/method/message/MessageTest.java
@@ -3,7 +3,9 @@ package com.axibase.tsd.api.method.message;
 
 import com.axibase.tsd.api.Checker;
 import com.axibase.tsd.api.method.checks.AbstractCheck;
+import com.axibase.tsd.api.method.checks.Check;
 import com.axibase.tsd.api.method.checks.MessageCheck;
+import com.axibase.tsd.api.method.entity.EntityMethod;
 import com.axibase.tsd.api.model.message.Message;
 import com.axibase.tsd.api.model.message.MessageQuery;
 import com.axibase.tsd.api.util.NotCheckedException;
@@ -16,22 +18,48 @@ import java.util.List;
 import static org.testng.AssertJUnit.fail;
 
 public class MessageTest extends MessageMethod {
-    public static void assertMessageExisting(String assertMessage, Message message) {
-        try {
-            Checker.check(new MessageCheck(message));
-        } catch (NotCheckedException e) {
-            fail(assertMessage);
-        }
-    }
 
     public static void assertMessageExisting(Message message) {
         String assertMessage = String.format(
                 DefaultMessagesTemplates.MESSAGE_NOT_EXIST,
                 message
         );
-        assertMessageExisting(assertMessage, message);
+        assertMessageExisting(assertMessage, message, true);
     }
 
+    public static void assertMessageExisting(String assertMessage, Message message) {
+        assertMessageExisting(assertMessage, message, true);
+    }
+
+    /**
+     * @param checkThatMessageEntityIsCreated If true, then get entity name from the {@param message},
+     *                                 and check that ATSD has entity with this name
+     *                                 before check the message itself.
+     */
+    public static void assertMessageExisting(String assertMessage, Message message, boolean checkThatMessageEntityIsCreated) {
+        if (checkThatMessageEntityIsCreated) {
+            assertEntity(message.getEntity());
+        }
+        assertMessage(assertMessage, message);
+    }
+
+    private static void assertEntity(String entityName) {
+        String errorMessage = String.format("ATSD does not know entity %s.", entityName);
+        Check entityNameCheck = new Check(errorMessage, () -> EntityMethod.entityExist(entityName));
+        try {
+            Checker.check(entityNameCheck);
+        } catch (NotCheckedException e) {
+            fail(errorMessage);
+        }
+    }
+
+    private static void assertMessage(String assertMessage, Message message) {
+        try {
+            Checker.check(new MessageCheck(message));
+        } catch (NotCheckedException e) {
+            fail(assertMessage);
+        }
+    }
 
     public static void assertMessageQuerySize(final MessageQuery query, final Integer size) {
         try {
