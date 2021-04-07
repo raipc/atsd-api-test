9faa3db44ff78a7721636f2e52f53dd08e171314
Alexander Tokarev <atokarev@gmail.com>
2021-04-07 20:27:01 +0300

    Test date_format filter for session_summary table (#708)
    * Test date_format filter for session_summary table
    
    * Test date_format filter for session_summary table: HH:mm pattern
    
    * Test date_format filter for session_summary table: test numeric comparison

--- src/test/java/com/axibase/tsd/api/method/sql/trade/session_summary/TradeSessionSummaryDateFormatFilterTest.java
+++ src/test/java/com/axibase/tsd/api/method/sql/trade/session_summary/TradeSessionSummaryDateFormatFilterTest.java
@@ -1,29 +1,40 @@
-package com.axibase.tsd.api.method.sql.trade;
+package com.axibase.tsd.api.method.sql.trade.session_summary;
 
-import com.axibase.tsd.api.model.financial.Trade;
+import com.axibase.tsd.api.method.sql.trade.SqlTradeTest;
+import com.axibase.tsd.api.method.trade.session_summary.TradeSessionSummaryMethod;
+import com.axibase.tsd.api.model.financial.TradeSessionStage;
+import com.axibase.tsd.api.model.financial.TradeSessionSummary;
+import com.axibase.tsd.api.model.financial.TradeSessionType;
 import com.axibase.tsd.api.util.TestUtil;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
-public class TradeDateFormatFilterTest extends SqlTradeTest {
+public class TradeSessionSummaryDateFormatFilterTest extends SqlTradeTest {
     public static final String QUERY =
-            "select datetime, open() from atsd_trade " +
-                    "where {instrument} and {time} and {date_format} " +
+            "select datetime from atsd_session_summary " +
+                    "where exchange = 'MOEX' and class = '{class}' and {date_format} " +
                     "group by exchange, class, symbol, period(1 day) " +
                     "WITH TIMEZONE = 'UTC'";
 
     @BeforeClass
     public void prepareData() throws Exception {
-        List<Trade> trades = new ArrayList<>();
-        trades.add(fromISOString("2020-04-16T15:55:15Z").setPrice(new BigDecimal("1")));
-        trades.add(fromISOString("2020-04-17T23:15:55Z").setPrice(new BigDecimal("2")));
-        trades.add(fromISOString("2020-04-18T23:00:00Z").setPrice(new BigDecimal("3")));
-        insert(trades);
+        insert(trade(1000).setExchange("MOEX"));
+
+        TradeSessionSummary sessionSummary = new TradeSessionSummary(clazz(), symbol(), TradeSessionType.MORNING, TradeSessionStage.N, "2020-04-16T15:55:15Z");
+        sessionSummary.addTag("rptseq", "2");
+        sessionSummary.addTag("offer", "10.5");
+        TradeSessionSummaryMethod.importStatistics(sessionSummary);
+
+        sessionSummary = new TradeSessionSummary(clazz(), symbol(), TradeSessionType.MORNING, TradeSessionStage.N, "2020-04-17T23:15:55Z");
+        sessionSummary.addTag("action", "test");
+        sessionSummary.addTag("starttime", "10:15:20");
+        TradeSessionSummaryMethod.importStatistics(sessionSummary);
+
+        sessionSummary = new TradeSessionSummary(clazz(), symbol(), TradeSessionType.DAY, TradeSessionStage.N, "2020-04-18T23:00:00Z");
+        sessionSummary.addTag("action", "test");
+        sessionSummary.addTag("starttime", "10:15:20");
+        TradeSessionSummaryMethod.importStatistics(sessionSummary);
     }
 
     @Test(dataProvider = "testData")
@@ -37,80 +48,76 @@ public class TradeDateFormatFilterTest extends SqlTradeTest {
         TestConfig[] data = {
                 test("Day pattern less than date_format function")
                         .dateFormat("date_format(time, 'dd') < '17'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1"),
+                        .addExpected("2020-04-16T00:00:00.000000Z"),
                 test("Day pattern less or equal date_format function")
                         .dateFormat("date_format(time, 'dd') <= '16'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1"),
+                        .addExpected("2020-04-16T00:00:00.000000Z"),
                 test("Day pattern  equal date_format function")
                         .dateFormat("date_format(time, 'dd') = '17'")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2"),
+                        .addExpected("2020-04-17T00:00:00.000000Z"),
                 test("Day pattern  not equal date_format function")
                         .dateFormat("date_format(time, 'dd') != '18'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2"),
+                        .addExpected("2020-04-16T00:00:00.000000Z")
+                        .addExpected("2020-04-17T00:00:00.000000Z"),
                 test("Day pattern greater than date_format function")
                         .dateFormat("date_format(time, 'dd') > '17'")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
+                        .addExpected("2020-04-18T00:00:00.000000Z"),
                 test("Day pattern greater or equal date_format function")
                         .dateFormat("date_format(time, 'dd') >= '17'")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
+                        .addExpected("2020-04-17T00:00:00.000000Z")
+                        .addExpected("2020-04-18T00:00:00.000000Z"),
 
                 test("Day pattern less than extract function")
                         .dateFormat("extract(day from time) < '17'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1"),
+                        .addExpected("2020-04-16T00:00:00.000000Z"),
                 test("Day pattern less or equal extract function")
                         .dateFormat("extract(day from time) <= '16'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1"),
+                        .addExpected("2020-04-16T00:00:00.000000Z"),
                 test("Day pattern  equal extract function")
                         .dateFormat("extract(day from time) = '17'")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2"),
+                        .addExpected("2020-04-17T00:00:00.000000Z"),
                 test("Day pattern  not equal extract function")
                         .dateFormat("extract(day from time) != '18'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2"),
+                        .addExpected("2020-04-16T00:00:00.000000Z")
+                        .addExpected("2020-04-17T00:00:00.000000Z"),
                 test("Day pattern greater than extract function")
                         .dateFormat("extract(day from time) > '17'")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
+                        .addExpected("2020-04-18T00:00:00.000000Z"),
                 test("Day pattern greater or equal extract function")
                         .dateFormat("extract(day from time) >= '17'")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
+                        .addExpected("2020-04-17T00:00:00.000000Z")
+                        .addExpected("2020-04-18T00:00:00.000000Z"),
 
                 test("HH:mm pattern not equal")
                         .dateFormat("date_format(time, 'HH:mm') != '23:15'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
+                        .addExpected("2020-04-16T00:00:00.000000Z")
+                        .addExpected("2020-04-18T00:00:00.000000Z"),
 
                 test("Minute pattern less than")
                         .dateFormat("date_format(time, 'mm') < '15'")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
+                        .addExpected("2020-04-18T00:00:00.000000Z"),
                 test("Minute pattern less or equal")
                         .dateFormat("date_format(time, 'mm') <= '15'")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
+                        .addExpected("2020-04-17T00:00:00.000000Z")
+                        .addExpected("2020-04-18T00:00:00.000000Z"),
                 test("Minute pattern  equal")
                         .dateFormat("date_format(time, 'mm') = '15'")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2"),
+                        .addExpected("2020-04-17T00:00:00.000000Z"),
                 test("Minute pattern not equal")
                         .dateFormat("date_format(time, 'mm') != '15'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
+                        .addExpected("2020-04-16T00:00:00.000000Z")
+                        .addExpected("2020-04-18T00:00:00.000000Z"),
                 test("Minute pattern greater than")
                         .dateFormat("date_format(time, 'mm') > '15'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1"),
+                        .addExpected("2020-04-16T00:00:00.000000Z"),
                 test("Minute pattern greater or equal")
                         .dateFormat("date_format(time, 'mm') >= '15'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2"),
+                        .addExpected("2020-04-16T00:00:00.000000Z")
+                        .addExpected("2020-04-17T00:00:00.000000Z"),
+
                 test("Day of week pattern")
                         .dateFormat("date_format(time, 'u') = '6'")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
-                test("Day of week pattern numeric comparison")
-                        .dateFormat("date_format(time, 'u') < '10'")
-                        .addExpected("2020-04-16T00:00:00.000000Z", "1")
-                        .addExpected("2020-04-17T00:00:00.000000Z", "2")
-                        .addExpected("2020-04-18T00:00:00.000000Z", "3"),
+                        .addExpected("2020-04-18T00:00:00.000000Z"),
         };
         return TestUtil.convertTo2DimArray(data);
     }
@@ -123,13 +130,9 @@ public class TradeDateFormatFilterTest extends SqlTradeTest {
 
         public TestConfig(String description) {
             super(description);
-            time("time between '2020-04-15T00:00:00Z' and '2020-04-19T00:00:00Z'");
+            setVariable("class", clazz());
         }
 
-        public TestConfig time(String time) {
-            setVariable("time", time);
-            return this;
-        }
 
         public TestConfig dateFormat(String dateFormat) {
             setVariable("date_format", dateFormat);
